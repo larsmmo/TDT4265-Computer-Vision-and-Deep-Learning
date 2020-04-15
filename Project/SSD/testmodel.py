3f3eb1f382a5822ea8d7d4c7aa3b35304ed9c2ad
larsmmo <larsmmo@stud.ntnu.no>
2020-04-15 20:50:41 +0200

    no mirror

--- Project/SSD/testmodel.py
+++ Project/SSD/testmodel.py
@@ -4,8 +4,8 @@ from torchvision import models
 from torchsummary import summary
 
 if __name__ == "__main__":
-    model = models.resnet18(pretrained = True)
+    model = models.resnext101_32x8d(pretrained = True)
 
     #features = nn.Sequential(*list(model.children())[:-2])
 
-    summary(model, (3, 300, 300))
+    summary(model, (3, 360, 270))
