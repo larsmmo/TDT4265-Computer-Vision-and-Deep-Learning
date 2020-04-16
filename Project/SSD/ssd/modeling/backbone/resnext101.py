e729a2714e452a9e89dbaf64ba8e4d7d88dfa8d1
larsmmo <larsmmo@stud.ntnu.no>
2020-04-16 21:02:46 +0200

    Fix residual blocks

--- Project/SSD/ssd/modeling/backbone/resnext101.py
+++ Project/SSD/ssd/modeling/backbone/resnext101.py
@@ -8,6 +8,7 @@ from torchvision import models
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, padding=1):
     """3x3 convolution with padding"""
+    print(padding)
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=padding, groups=groups, bias=False, dilation=dilation)
 
