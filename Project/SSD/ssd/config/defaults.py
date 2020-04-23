918457539978a2587f4c7577a89316802d01b809
larsmmo <larsmmo@stud.ntnu.no>
2020-04-23 17:03:32 +0200

    changed save step

--- Project/SSD/ssd/config/defaults.py
+++ Project/SSD/ssd/config/defaults.py
@@ -105,7 +105,7 @@ cfg.TEST.BATCH_SIZE = 10
 # Specific test options
 # ---------------------------------------------------------------------------- #
 cfg.EVAL_STEP = 500 # Evaluate dataset every eval_step, disabled when eval_step < 0
-cfg.MODEL_SAVE_STEP = 500 # Save checkpoint every save_step
+cfg.MODEL_SAVE_STEP = 2500 # Save checkpoint every save_step
 cfg.LOG_STEP = 10 # Print logs every log_stepPrint logs every log_step
 cfg.OUTPUT_DIR = "outputs"
 cfg.DATASET_DIR = "datasets"
